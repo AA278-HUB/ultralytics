594d0272aad566f04e01ade22cac013fdbfbc364
li si <2782453092@qq.com>
2025-10-22 17:44:00 +0800

    成功添加了检测头

---
+++ Custom_Train/1_train_yolo11n_vehicle_orientation_300_epoch.py
@@ -0,0 +1,22 @@
+# coding:utf-8      `
+from datetime import datetime
+from ultralytics import YOLO
+
+# 模型配置文件
+model_yaml_path = "Custom_Model_cfg/yolo11n.yaml"
+# data="coco8.yaml"
+data = "Custom_dataset_cfg/coco-vehicle.yaml"
+# 预训练模型
+if __name__ == '__main__':
+    # 加载预训练模型
+    # model =YOLO(model_yaml_path)
+    model = YOLO(model_yaml_path)
+
+    results = model.train(data=data,
+                          epochs=10,
+                          batch=8,
+                          imgsz=640,
+                          cos_lr=True,
+                          close_mosaic=50,
+                          name="test"+datetime.now().strftime("%Y%m%d_%H_%M"))
+
