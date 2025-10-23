3012abb89389880622028e408e06bede14a33467
li si <2782453092@qq.com>
2025-10-23 22:15:31 +0800

    添加了Ghost

---
+++ Custom_Train/2_train_yolo11_ghost_vehicle_orientation_200_epoch.py
@@ -0,0 +1,24 @@
+# coding:utf-8      `
+from datetime import datetime
+from ultralytics import YOLO
+
+# 模型配置文件
+model_yaml_path = "Custom_Model_cfg/yolo11_Ghost.yaml"
+# data="coco8.yaml"
+data = "Custom_dataset_cfg/vehicle_orientation.yaml"
+# 预训练模型
+if __name__ == '__main__':
+    # 加载预训练模型
+    # model =YOLO(model_yaml_path)
+    model = YOLO(model_yaml_path)
+
+    results = model.train(data=data,
+                          epochs=200,
+                          batch=8,
+                          imgsz=640,
+                          cos_lr=True,
+                          close_mosaic=50,
+                          save=True,
+                          device="0",
+                          name="yolo11_ghost"+datetime.now().strftime("%Y%m%d_%H_%M"))
+
