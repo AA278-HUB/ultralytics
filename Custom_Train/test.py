e2ef8be863cc6dd3f2ce2ed9bc30b212d91991b5
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2025-10-27 15:06:12 +0800

    加入C3repghost测试

--- Custom_Train/test.py
+++ Custom_Train/test.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 # 模型配置文件
 model_yaml_path = "Custom_Model_cfg/yolo11_dyhead.yaml"
-model_yaml_path = "Custom_Model_cfg/yolo11_Ghost.yaml"
+model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_1.yaml"
 
 data="coco8.yaml"
 data = "Custom_dataset_cfg/coco-vehicle.yaml"
