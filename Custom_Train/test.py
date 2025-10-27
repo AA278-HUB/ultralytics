6f3891e89fe7143d361046904b223337c2870a05
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2025-10-27 15:28:57 +0800

    准备测试REP

--- Custom_Train/test.py
+++ Custom_Train/test.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 # 模型配置文件
 model_yaml_path = "Custom_Model_cfg/yolo11_dyhead.yaml"
 model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_1.yaml"
-
+model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_Rep.yaml"
 data="coco8.yaml"
 data = "Custom_dataset_cfg/coco-vehicle.yaml"
 # 预训练模型
