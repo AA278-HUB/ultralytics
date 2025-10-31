d8b830b42075030ab77141aa090aa852d4da8938
li si <2782453092@qq.com>
2025-10-31 21:41:51 +0800

    修改了shufflev2

--- Custom_Train/6_train_yolo11n_Rep_shufflev2_vehicle_orientation_200_epoch.py
+++ Custom_Train/6_train_yolo11n_Rep_shufflev2_vehicle_orientation_200_epoch.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 
 # 模型配置文件
-model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_Rep_shuffle_2.yaml"
+model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_Rep_shufflev2.yaml"
 # data="coco8.yaml"
 data = "Custom_dataset_cfg/vehicle_orientation.yaml"
 # 预训练模型
@@ -15,10 +15,10 @@ if __name__ == '__main__':
 
     results = model.train(data=data,
                           epochs=200,
-                          batch=64,
+                          batch=32,
                           imgsz=640,
                           cos_lr=True,
                           close_mosaic=50,
                           save=True,
                           device="0",
-                          name="yolo11_Rep_shufflev1"+datetime.now().strftime("%Y%m%d_%H_%M"))
+                          name="yolo11_Rep_shufflev2_new"+datetime.now().strftime("%Y%m%d_%H_%M"))
--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2278,6 +2278,8 @@ class ShuffleV1Block(nn.Module):
         return x
 
 
+import torch
+import torch.nn as nn
 
 class ShuffleV2Block(nn.Module):
     def __init__(self, inp, oup, mid_channels, ksize, stride):
@@ -2290,52 +2292,30 @@ class ShuffleV2Block(nn.Module):
         pad = ksize // 2
         self.pad = pad
         self.inp = inp
-        if stride==1:
-            outputs=oup -inp//2
-        else:
-            outputs = oup - inp
-        #
-        # branch_main = [
-        #     # pw
-        #     nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
-        #     nn.BatchNorm2d(mid_channels),
-        #     nn.ReLU(inplace=True),
-        #     # dw
-        #     nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
-        #     nn.BatchNorm2d(mid_channels),
-        #     # pw-linear
-        #     nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
-        #     nn.BatchNorm2d(outputs),
-        #     nn.ReLU(inplace=True),
-        # ]
 
+        outputs = oup - inp
 
-        if stride ==1:
+        if stride == 1:
+            assert inp == oup
             branch_main = [
-                # pw
-                nn.Conv2d(inp//2, mid_channels, 1, 1, 0, bias=False),
+                nn.Conv2d(inp // 2, mid_channels, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(mid_channels),
                 nn.ReLU(inplace=True),
-                # dw
                 nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
                 nn.BatchNorm2d(mid_channels),
-                # pw-linear
-                nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(outputs),
+                nn.Conv2d(mid_channels, inp // 2, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(inp // 2),
                 nn.ReLU(inplace=True),
             ]
         else:
             branch_main = [
-                # pw
                 nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(mid_channels),
                 nn.ReLU(inplace=True),
-                # dw
                 nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
                 nn.BatchNorm2d(mid_channels),
-                # pw-linear
-                nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(outputs),
+                nn.Conv2d(mid_channels, oup - inp, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup - inp),
                 nn.ReLU(inplace=True),
             ]
 
@@ -2356,14 +2336,14 @@ class ShuffleV2Block(nn.Module):
             self.branch_proj = None
 
     def forward(self, old_x):
-        if self.stride==1:
+        if self.stride == 1:
             x_proj, x = self.channel_shuffle(old_x)
-            x_temp=self.branch_main(x)
-            return torch.cat((x_proj, x_temp), 1)
-        elif self.stride==2:
+            return torch.cat((x_proj, self.branch_main(x)), 1)
+        elif self.stride == 2:
             x_proj = old_x
             x = old_x
-            return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+            x = torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+            return x
 
     def channel_shuffle(self, x):
         batchsize, num_channels, height, width = x.data.size()
@@ -2372,3 +2352,103 @@ class ShuffleV2Block(nn.Module):
         x = x.permute(1, 0, 2)
         x = x.reshape(2, -1, num_channels // 2, height, width)
         return x[0], x[1]
+
+
+
+
+##=============这个是被我瞎改过的ShuffleV2Block=====================
+##=============要么通道上升补上增大，要么=====================
+
+# class ShuffleV2Block(nn.Module):
+#     def __init__(self, inp, oup, mid_channels, ksize, stride):
+#         super(ShuffleV2Block, self).__init__()
+#         self.stride = stride
+#         assert stride in [1, 2]
+#
+#         self.mid_channels = mid_channels
+#         self.ksize = ksize
+#         pad = ksize // 2
+#         self.pad = pad
+#         self.inp = inp
+#         if stride==1:
+#             outputs=oup -inp//2
+#         else:
+#             outputs = oup - inp
+#         #
+#         # branch_main = [
+#         #     # pw
+#         #     nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+#         #     nn.BatchNorm2d(mid_channels),
+#         #     nn.ReLU(inplace=True),
+#         #     # dw
+#         #     nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
+#         #     nn.BatchNorm2d(mid_channels),
+#         #     # pw-linear
+#         #     nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+#         #     nn.BatchNorm2d(outputs),
+#         #     nn.ReLU(inplace=True),
+#         # ]
+#
+#
+#         if stride ==1:
+#             branch_main = [
+#                 # pw
+#                 nn.Conv2d(inp//2, mid_channels, 1, 1, 0, bias=False),
+#                 nn.BatchNorm2d(mid_channels),
+#                 nn.ReLU(inplace=True),
+#                 # dw
+#                 nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
+#                 nn.BatchNorm2d(mid_channels),
+#                 # pw-linear
+#                 nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+#                 nn.BatchNorm2d(outputs),
+#                 nn.ReLU(inplace=True),
+#             ]
+#         else:
+#             branch_main = [
+#                 # pw
+#                 nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+#                 nn.BatchNorm2d(mid_channels),
+#                 nn.ReLU(inplace=True),
+#                 # dw
+#                 nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
+#                 nn.BatchNorm2d(mid_channels),
+#                 # pw-linear
+#                 nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+#                 nn.BatchNorm2d(outputs),
+#                 nn.ReLU(inplace=True),
+#             ]
+#
+#         self.branch_main = nn.Sequential(*branch_main)
+#
+#         if stride == 2:
+#             branch_proj = [
+#                 # dw
+#                 nn.Conv2d(inp, inp, ksize, stride, pad, groups=inp, bias=False),
+#                 nn.BatchNorm2d(inp),
+#                 # pw-linear
+#                 nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
+#                 nn.BatchNorm2d(inp),
+#                 nn.ReLU(inplace=True),
+#             ]
+#             self.branch_proj = nn.Sequential(*branch_proj)
+#         else:
+#             self.branch_proj = None
+#
+#     def forward(self, old_x):
+#         if self.stride==1:
+#             x_proj, x = self.channel_shuffle(old_x)
+#             x_temp=self.branch_main(x)
+#             return torch.cat((x_proj, x_temp), 1)
+#         elif self.stride==2:
+#             x_proj = old_x
+#             x = old_x
+#             return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
+#
+#     def channel_shuffle(self, x):
+#         batchsize, num_channels, height, width = x.data.size()
+#         assert (num_channels % 4 == 0)
+#         x = x.reshape(batchsize * num_channels // 2, 2, height * width)
+#         x = x.permute(1, 0, 2)
+#         x = x.reshape(2, -1, num_channels // 2, height, width)
+#         return x[0], x[1]
