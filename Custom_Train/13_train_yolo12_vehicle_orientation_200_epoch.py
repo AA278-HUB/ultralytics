46107b4c131b28dcfe556a975a63915c6dc972f5
li si <2782453092@qq.com>
2025-12-17 17:34:53 +0800

    geng xin

--- Custom_Train/13_train_yolo12_vehicle_orientation_200_epoch.py
+++ Custom_Train/13_train_yolo12_vehicle_orientation_200_epoch.py
@@ -13,7 +13,9 @@ from datetime import datetime
 from ultralytics import YOLO
 # 模型配置文件
 
-model_yaml_path = "Custom_Model_cfg/yolo12n.yaml"
+# model_yaml_path = "Custom_Model_cfg/yolo12n.yaml"
+model_yaml_path = r"C:\Users\Hunger\Desktop\ultralytics\runs\detect\yolo12n_20251215_15_37\weights\last.pt"
+
 # data="coco8.yaml"
 data = "Custom_dataset_cfg/vehicle_orientation.yaml"
 # 预训练模型
@@ -21,14 +23,14 @@ if __name__ == '__main__':
     # 加载预训练模型
     # model =YOLO(model_yaml_path)
     model = YOLO(model_yaml_path)
-
-    results = model.train(data=data,
-                          epochs=200,
-                          batch=16,
-                          imgsz=640,
-                          cos_lr=True,
-                          close_mosaic=50,
-                          save=True,
-                          save_period=10,
-                          device="0",
-                          name="yolo12n"+"_"+datetime.now().strftime("%Y%m%d_%H_%M"))
+    results = model.train(resume=True)
+    # results = model.train(data=data,
+    #                       epochs=200,
+    #                       batch=16,
+    #                       imgsz=640,
+    #                       cos_lr=True,
+    #                       close_mosaic=50,
+    #                       save=True,
+    #                       save_period=10,
+    #                       device="0",
+    #                       name="yolo12n"+"_"+datetime.now().strftime("%Y%m%d_%H_%M"))
