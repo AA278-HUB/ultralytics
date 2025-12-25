06b35833007e8ca6e9305ee7a5ac34447e689c1d
li si <2782453092@qq.com>
2025-12-25 18:17:11 +0800

    ss

--- Custom_Train/19_train_yolo11n_Rep_shufflev2_True_vehicle_orientation_200_epoch.py
+++ Custom_Train/19_train_yolo11n_Rep_shufflev2_True_vehicle_orientation_200_epoch.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     results = model.train(data=data,
                           epochs=200,
-                          batch=64,
+                          batch=32,
                           imgsz=640,
                           cos_lr=True,
                           close_mosaic=50,
