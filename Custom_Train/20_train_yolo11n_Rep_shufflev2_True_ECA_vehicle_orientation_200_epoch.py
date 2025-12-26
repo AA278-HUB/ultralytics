91cb5841ccbf0571399032c7827075eae7ed2d16
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2025-12-26 17:15:54 +0800

    boring

---
+++ Custom_Train/20_train_yolo11n_Rep_shufflev2_True_ECA_vehicle_orientation_200_epoch.py
@@ -0,0 +1,25 @@
+# coding:utf-8      `
+from datetime import datetime
+from ultralytics import YOLO
+
+
+# 模型配置文件
+model_yaml_path = r"Custom_Model_cfg/yolo11_Ghost_Rep_shufflev2_True_CA.yaml"
+# model_yaml_path =r"C:\Users\Hunger\Desktop\ultralytics\runs\detect\yolo11_Ghost_Rep_shuffle2_True20251225_18_17\weights\last.pt"
+# data="coco8.yaml"
+data = "Custom_dataset_cfg/vehicle_orientation.yaml"
+# 预训练模型
+if __name__ == '__main__':
+    # 加载预训练模型
+    # model =YOLO(model_yaml_path)
+    model = YOLO(model_yaml_path)
+    # results=model.train(resume=True)
+    results = model.train(data=data,
+                          epochs=200,
+                          batch=32,
+                          imgsz=640,
+                          cos_lr=True,
+                          close_mosaic=50,
+                          save=True,
+                          device="0",
+                          name="yolo11_Ghost_Rep_shuffle2_True"+datetime.now().strftime("%Y%m%d_%H_%M"))
