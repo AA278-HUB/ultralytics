dac94b2a859e86f2655e71116d78aeacb67ad0d1
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2025-12-27 12:44:22 +0800

    boring

--- Custom_Train/18_train_yolo11_Ghost_Rep_shufflev2_ECA_vehicle_orientation_200_epoch.py
+++ Custom_Train/18_train_yolo11_Ghost_Rep_shufflev2_ECA_vehicle_orientation_200_epoch.py
@@ -17,6 +17,7 @@ from ultralytics import YOLO
 # 模型配置文件
 
 model_yaml_path = "Custom_Model_cfg/yolo11_Ghost_Rep_shufflev2_ECA.yaml"
+model_yaml_path = r"C:\Users\Hunger\Desktop\ultralytics\runs\detect\yolo11_Ghost_Rep_shufflev2_ECA20251224_19_22\weights\last.pt"
 # data="coco8.yaml"
 data = "Custom_dataset_cfg/vehicle_orientation.yaml"
 # 预训练模型
@@ -24,14 +25,14 @@ if __name__ == '__main__':
     # 加载预训练模型
     # model =YOLO(model_yaml_path)
     model = YOLO(model_yaml_path)
-
-    results = model.train(data=data,
-                          epochs=200,
-                          batch=32,
-                          imgsz=640,
-                          cos_lr=True,
-                          close_mosaic=50,
-                          save=True,
-                          save_period=1,
-                          device="0",
-                          name="yolo11_Ghost_Rep_shufflev2_ECA"+datetime.now().strftime("%Y%m%d_%H_%M"))
+    results = model.train(resume=True)
+    # results = model.train(data=data,
+    #                       epochs=200,
+    #                       batch=32,
+    #                       imgsz=640,
+    #                       cos_lr=True,
+    #                       close_mosaic=50,
+    #                       save=True,
+    #                       save_period=1,
+    #                       device="0",
+    #                       name="yolo11_Ghost_Rep_shufflev2_ECA"+datetime.now().strftime("%Y%m%d_%H_%M"))
