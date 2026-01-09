14db4ceee2259cf26eafa12fb3a13c4d1369bd66
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-01-09 10:55:04 +0800

    +了一堆垃圾

--- Custom_Train3/3_4_train_yolo11n_RepViT_Ghost_shufflev2_EMA_CA.py
+++ Custom_Train3/3_4_train_yolo11n_RepViT_Ghost_shufflev2_EMA_CA.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 
 # 模型配置文件
-model_yaml_path = r"Custom_Model_cfg_3/yolo11_RepViTBlock_Ghost_shufflev2_EMA.yaml"
+model_yaml_path = r"Custom_Model_cfg_3/yolo11_RepViTBlock_Ghost_shufflev2_EMA_CA.yaml"
 # model_yaml_path =r"C:\Users\Hunger\Desktop\ultralytics\runs\detect\yolo11_Ghost_Rep_shuffle2_True_CA20251226_17_20\weights\last.pt"
 # data="coco8.yaml"
 data = "Custom_dataset_cfg/vehicle_orientation.yaml"
@@ -22,4 +22,4 @@ if __name__ == '__main__':
                           close_mosaic=50,
                           save=True,
                           device=[0,1,2,3],
-                          name="yolo11_RepViTBlock_Ghost_shufflev2_EMA"+datetime.now().strftime("%Y%m%d_%H_%M"))
+                          name="yolo11_RepViTBlock_Ghost_shufflev2_EMA_CA"+datetime.now().strftime("%Y%m%d_%H_%M"))
