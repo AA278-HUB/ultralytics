fbd10929b1f90e9fd8049d2ca02d9fff0fc82962
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-01-12 14:30:18 +0800

    噩梦

---
+++ Custom_Train4/4_1_yolo11n_minidateset.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+from datetime import datetime
+from ultralytics import YOLO
+import os
+
+# 模型配置文件
+model_yaml_paths = [
+           r"Custom_Model_cfg_4/yolo11n.yaml",
+           r"Custom_Model_cfg_4/yolo11_bifpn.yaml",
+           r"Custom_Model_cfg_4/yolo11_bifpn_2.yaml",
+           r"Custom_Model_cfg_4/yolo11_C3RepGhost.yaml"
+                    ]
+data = "Custom_dataset_cfg/vehicle_orientation_mini.yaml"
+
+if __name__ == '__main__':
+    for model_yaml_path in model_yaml_paths:
+        # 模型名（不含路径和后缀）
+        model_name = os.path.splitext(os.path.basename(model_yaml_path))[0]
+
+        print(f"\n==== Training model: {model_name} ====\n")
+
+        model = YOLO(model_yaml_path)
+
+        model.train(
+            data=data,
+            epochs=200,
+            batch=64,
+            imgsz=640,  # 保持不变
+            cos_lr=True,
+            lr0=0.02,  # ↑ 初始学习率
+            lrf=0.1,  # ↑ 最终学习率比例
+            close_mosaic=20,  # 提前关闭 mosaic
+            save=True,
+            device=[0, 1, 2, 3],
+            name=f"{model_name}_{datetime.now().strftime('%Y%m%d_%H_%M')}"
+        )
+
---
+++ Custom_Train4/test.py
@@ -0,0 +1,41 @@
+# coding:utf-8      `
+from datetime import datetime
+from ultralytics import YOLO
+
+
+
+# 模型配置文件
+model_yaml_paths = [
+           r"Custom_Model_cfg_4/yolo11n.yaml",
+           r"Custom_Model_cfg_4/yolo11_bifpn.yaml",
+           r"Custom_Model_cfg_4/yolo11_bifpn_2.yaml",
+           r"Custom_Model_cfg_4/yolo11_C3RepGhost.yaml"
+                    ]
+
+# data="coco8.yaml"
+#data = "Custom_dataset_cfg/coco-vehicle.yaml"
+# 预训练模型
+if __name__ == '__main__':
+
+    for path in model_yaml_paths:
+        model = YOLO(path)
+        # results = model.train(data=data,
+        #                       epochs=3,
+        #                       batch=8,
+        #                       imgsz=640,
+        #                       cos_lr=True,
+        #                       close_mosaic=50,
+        #                       save= True,
+        #                       device=-1,
+        #                       workers=16,
+        #                       name="test"+datetime.now().strftime("%Y%m%d_%H_%M"))
+
+        print("训练完成后打印参数")
+
+        model.info()
+        # 部署准备
+        model.fuse()
+        model.eval()
+        # model.val()
+        print("训练结束打印参数")
+        model.info()
