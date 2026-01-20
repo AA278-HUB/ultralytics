47d544c258eab7eeb195c7d62afa9c33b9f2d2c3
li si <2782453092@qq.com>
2026-01-20 15:25:24 +0800

    mamba

--- Custom_Distiller/test.py
+++ Custom_Distiller/test.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     # 1. 加载模型
     # -------------------------
     student = YOLO(r"DistillModel/yolo11.yaml")
-    teacher = YOLO(r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Distiller\yolo11n.pt")  # 必须同尺度
+    teacher = YOLO(r"C:\Users\Hunger\Desktop\ultralytics\Custom_Distiller\best.pt")  # 必须同尺度
     teacher.model.eval()
     for p in teacher.model.parameters():
         p.requires_grad = False
     student.model=DetectionModelWithKD(cfg=r"DistillModel/yolo11.yaml",ch=3,nc=4,verbose=False,teacher=teacher,kd_weight=1)
-    student.train(data="coco8.yaml",epochs=3,Distill=True,model=student)
+    student.train(data="Custom_dataset_cfg/vehicle_orientation_mini.yaml",epochs=3,Distill=True,model=student)
