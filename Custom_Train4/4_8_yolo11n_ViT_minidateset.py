b855877fcc65943263045674b50d63cff6789c75
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-01-27 16:13:30 +0800

    改进

--- Custom_Train4/4_8_yolo11n_ViT_minidateset.py
+++ Custom_Train4/4_8_yolo11n_ViT_minidateset.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
             lrf=0.1,  # ↑ 最终学习率比例
             close_mosaic=20,  # 提前关闭 mosaic
             save=True,
-            device=-1,
+            device=0,
             amp=False,
             name=f"{model_name}_{datetime.now().strftime('%Y%m%d_%H_%M')}"
         )
