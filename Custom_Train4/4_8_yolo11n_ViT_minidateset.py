d762329a9390e42091f956fe7d51a406179dcc26
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-01-30 13:45:34 +0800

    改进

--- Custom_Train4/4_8_yolo11n_ViT_minidateset.py
+++ Custom_Train4/4_8_yolo11n_ViT_minidateset.py
@@ -28,13 +28,13 @@ model_yaml_paths = [
     #             r"Custom_Model_cfg_7/yolo11_MAFPN_modify.yaml",
     #             "Custom_Model_cfg_7/yolo11_MAFPN_modify_C3k2.yaml",
     #             r"Custom_Model_cfg_7/yolo11_MAFPN_modifyX_C3k2.yaml"
-    # r"Custom_Model_cfg_7/yolo11_Ghost_Rep_Ghost_shufflev2_MAFPN.yaml",
-    # r"Custom_Model_cfg_7/yolo11_Ghost_Rep_Ghost_shufflev2_CA_MAFPN.yaml",
+    r"Custom_Model_cfg_7/yolo11_Ghost_Rep_Ghost_shufflev2_MAFPN.yaml",
+    r"Custom_Model_cfg_7/yolo11_Ghost_Rep_Ghost_shufflev2_CA_MAFPN.yaml",
     # r"Custom_Model_cfg_7/yolo11-C3k2-iRMB.yaml",
     # r"Custom_Model_cfg_7/yolo11-C3k2-MambaOut.yaml",
     # r"Custom_Model_cfg_7/yolo11-C3k2-Faster-EMA.yaml",
     # r"Custom_Model_cfg_7/yolo11-C3k2-Star-CAA.yaml",
-    "Custom_Model_cfg_7/yolo11n.yaml"
+    # "Custom_Model_cfg_7/yolo11n.yaml"
     # r"/sysv/vehicle_orientation_mini/ultralytics/runs/detect/yolo11x_20260128_13_26/weights/last.pt"
     #  "Custom_Model_cfg_7/yolo11_RepStar.yaml"
     # "Custom_Model_cfg_7/yolo11-C3k2-LSBlock.yaml",
