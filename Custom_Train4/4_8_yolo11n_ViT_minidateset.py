1c30a35584a060b31f98582391e85b3b05a67e11
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-02-15 23:40:31 +0800

    改进

--- Custom_Train4/4_8_yolo11n_ViT_minidateset.py
+++ Custom_Train4/4_8_yolo11n_ViT_minidateset.py
@@ -147,10 +147,10 @@ if __name__ == '__main__':
         model.train(
             data=data,
             epochs=200,
-            batch=128,
+            batch=64,
             imgsz=640,  # 保持不变
             cos_lr=True,
-            lr0=0.01,  # ↑ 初始学习率
+            lr0=0.005,  # ↑ 初始学习率
             lrf=0.1,  # ↑ 最终学习率比例
             close_mosaic=20,  # 提前关闭 mosaic
             save=True,
