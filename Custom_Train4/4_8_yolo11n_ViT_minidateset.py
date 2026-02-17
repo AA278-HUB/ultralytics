32a5862ff99a67612898a95152290cf0368ffad9
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-02-17 19:06:57 +0800

    改进

--- Custom_Train4/4_8_yolo11n_ViT_minidateset.py
+++ Custom_Train4/4_8_yolo11n_ViT_minidateset.py
@@ -132,7 +132,8 @@ model_yaml_paths = [
 
     # "Custom_Model_cfg_12/yolo11_SGLK.yaml"
     # "Custom_Model_cfg_12/yolo11_DBSGLK.yaml"
-    "Custom_Model_cfg_12/yolo11_UniRep.yaml"
+    # "Custom_Model_cfg_12/yolo11_UniRep.yaml"
+    "Custom_Model_cfg_12/yolo11_StarRepLK.yaml",
 
 ]
 data = "Custom_dataset_cfg/vehicle_orientation_mini.yaml"
