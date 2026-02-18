3e703aca8508256343efd8fd7af9850f6c422800
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-02-18 12:45:31 +0800

    改进

--- Custom_Train4/test.py
+++ Custom_Train4/test.py
@@ -127,7 +127,8 @@ model_yaml_paths = [
     # "Custom_Model_cfg_12/yolo11_StarRepLK.yaml",
     # "Custom_Model_cfg_12/yolo11_AdvUniRepLK.yaml",
     # "Custom_Model_cfg_12/yolo11_SuperUniRepLK_CA.yaml",
-    "Custom_Model_cfg_12/yolo11_UniRep_2.yaml"
+    # "Custom_Model_cfg_12/yolo11_UniRep_2.yaml"
+    "Custom_Model_cfg_12/yolo11_MAFPN_modifyX_C3k2.yaml"
 
 ]
 
