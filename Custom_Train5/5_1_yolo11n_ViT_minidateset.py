5bb4312bcf654fadf5a786f01258edd88bddc1c3
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-03-01 10:53:14 +0800

    bbox_focaler_iou

---
+++ Custom_Train5/5_1_yolo11n_ViT_minidateset.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+from datetime import datetime
+from ultralytics import YOLO
+import os
+
+# 模型配置文件
+model_yaml_paths = [
+    "Custom_Model_cfg_13/yolo11_MAFPN_modifyX_uniRepLK_v1.yaml"
+
+]
+data = "Custom_dataset_cfg/vehicle_orientation_mini.yaml"
+
+if __name__ == '__main__':
+    for model_yaml_path in model_yaml_paths:
+        # 模型名（不含路径和后缀）
+        model_name = os.path.splitext(os.path.basename(model_yaml_path))[0]
+        print(f"\n==== Training model: {model_name} ====\n")
+        model = YOLO(model_yaml_path)
+        # model.train(resume=True)
+        Loss="bbox_mpdiou"
+# =======
+        model.train(
+            data=data,
+            epochs=200,
+            batch=64,
+            imgsz=640,  # 保持不变
+            cos_lr=True,
+            lr0=0.005,  # ↑ 初始学习率
+            lrf=0.2,  # ↑ 最终学习率比例
+            close_mosaic=20,  # 提前关闭 mosaic
+            save=True,
+            device=[0,1,2,3],
+            # amp=False,
+            name=f"{model_name}_{datetime.now().strftime('%Y%m%d_%H_%M')}_loss_{Loss}"
+        )
+# >>>>>>> fe705cf0be66db3253a39dab4347b0f99ce842c5
+
--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -12,7 +12,7 @@ from ultralytics.utils.metrics import OKS_SIGMA, calculate_siou
 from ultralytics.utils.ops import crop_mask, xywh2xyxy, xyxy2xywh
 from ultralytics.utils.tal import RotatedTaskAlignedAssigner, TaskAlignedAssigner, dist2bbox, dist2rbox, make_anchors
 from ultralytics.utils.torch_utils import autocast
-from .Mymetrics import WiseIouLoss, wasserstein_loss, bbox_inner_iou, bbox_focaler_iou
+from .Mymetrics import WiseIouLoss, wasserstein_loss, bbox_inner_iou, bbox_focaler_iou, bbox_mpdiou
 
 from .metrics import bbox_iou, probiou
 from .tal import bbox2dist
@@ -151,9 +151,9 @@ class BboxLoss(nn.Module):
             # iou = calculate_siou(pred_bboxes[fg_mask], target_bboxes[fg_mask])
 
             # iou = bbox_inner_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True, ratio=0.7)
-            # iou = bbox_mpdiou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, mpdiou_hw=mpdiou_hw[fg_mask])
+            iou = bbox_mpdiou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, mpdiou_hw=mpdiou_hw[fg_mask])
             # iou = bbox_inner_mpdiou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, mpdiou_hw=mpdiou_hw[fg_mask], ratio=0.7)
-            iou = bbox_focaler_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True, d=0.0, u=0.95)
+            # iou = bbox_focaler_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True, d=0.0, u=0.95)
             # iou = bbox_focaler_mpdiou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, mpdiou_hw=mpdiou_hw[fg_mask], d=0.0, u=0.95)
             loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
