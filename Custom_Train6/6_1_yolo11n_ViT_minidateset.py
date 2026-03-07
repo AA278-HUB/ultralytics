6f7b05ac3fde4453269855ce8c5280ae8bb614b8
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-03-07 11:57:26 +0800

    tongk

--- Custom_Train6/6_1_yolo11n_ViT_minidateset.py
+++ Custom_Train6/6_1_yolo11n_ViT_minidateset.py
@@ -17,7 +17,7 @@ CLS_MAP = {
 IOU_TYPES = ["CIoU","D-InterpIoU","InterpIoU","GIoU", "DIoU", "EIoU", "SIoU", "ShapeIoU", "WIoU", "PIoU", "PIoU2", "Inner_MPDIoU", "MPDIoU",
              "Focaler_MPDIoU","alpha_IoU"]  #
 
-IOU_TYPES["alpha_IoU"]
+IOU_TYPES=["alpha_IoU"]
 # 增强插件 (None: 保持原样, Inner: 内部辅助框, Focaler: 难样本聚焦)
 ENHANCE_TYPES = ["None",]  # "Inner", "Focaler"
 
