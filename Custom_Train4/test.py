3bd654b90ba66c2ca35211cb2d3b056961ae8413
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-03-07 21:54:45 +0800

    tongk

--- Custom_Train4/test.py
+++ Custom_Train4/test.py
@@ -151,7 +151,9 @@ model_yaml_paths = [
     # "Custom_Model_cfg_14/yolo11_MAFPN_modifyX_uniRepLK_v2.yaml",
     # "Custom_Model_cfg_14/yolo11_MAFPN_modifyX_uniRepLKv5_v1.yaml",
     # "Custom_Model_cfg_14/yolo11_MAFPN_modifyX_uniRepLK_v2.yaml"
-    "Custom_Model_cfg_15/yolo11_MAFPN_modifyX_uniRepLKv5_v2.yaml"
+    # "Custom_Model_cfg_15/yolo11_MAFPN_modifyX_uniRepLKv5_v2.yaml"
+    # "Custom_Model_cfg_12/yolo11n_UniRepLKv5.yaml"
+    "Custom_Model_cfg_15/yolo11_MAFPN_modifyX_uniRepLKv5_v3.yaml"
 ]
 
 cls_type="EMASlideLoss"
--- Custom_Val/val_feature.py
+++ Custom_Val/val_feature.py
@@ -1,11 +1,14 @@
 from ultralytics import YOLO
 
 
-path=r"D:\QQData\实验数据_new\yolo11n20251022_19_29\weights\best.pt"
 path=r"C:\Users\AAAAA\Desktop\detect\yolo11_MAFPN_modifyX_C3k2_20260126_14_33\weights\best.pt"
-# path=r"C:\Users\AAAAA\Desktop\李鬼\yolo11_MAFPN_modifyX_uniRepLKv5_v2_20260305_19_18_EMASlide__PIoU\weights\best.pt"
-test_image= r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\9e9546261431b15a7ca8961b970c1314.jpeg"
-test_image=r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\img_3.png"
+path=r"C:\Users\AAAAA\Desktop\李鬼\yolo11_MAFPN_modifyX_uniRepLKv5_v2_20260305_19_18_EMASlide__PIoU\weights\best.pt"
+path=r"D:\QQData\实验数据_new\yolo11n20251022_19_29\weights\best.pt"
+
+
+test_image= r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\38a0d47afe32a405dfb24668425c5db7_720.jpg"
+
+# test_image=r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\f4ffd76e78336c3e3d79fe7a4582ccf2_720.jpg"
 # test_image=r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\17018bf07d4c620980d539650c6b228f_720.jpg"
 # test_image=r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\06db2f27cc705f85f61eafbead89079b_720.jpg"
 # test_image =r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\1e596cc117da0abff85da08dcf39e9fe_720.jpg"
@@ -14,7 +17,7 @@ test_image=r"C:\Users\AAAAA\Desktop\ultralytics\Custom_Val\img_3.png"
 model = YOLO(path)
 results = model.predict(
     source=test_image,  # 也可以是图片文件夹路径
-    conf=0.5,  # 置信度阈值
+    conf=0.1,  # 置信度阈值
     save=True,  # 保存预测结果图
     # visualize=True  # 关键！开启特征可视化
 )
