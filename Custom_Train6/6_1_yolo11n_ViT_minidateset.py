0ff56b04306639d613869dccf5820b35951ace5e
YING ZENG <68038735+AA278-HUB@users.noreply.github.com>
2026-03-11 13:25:18 +0800

    tongk

--- Custom_Train6/6_1_yolo11n_ViT_minidateset.py
+++ Custom_Train6/6_1_yolo11n_ViT_minidateset.py
@@ -53,7 +53,7 @@ model_yaml_paths = [
     # "Custom_Model_cfg/yolov6.yaml",
     # "Custom_Model_cfg/yolov8.yaml",
     # "Custom_Model_cfg/yolov9t.yaml",
-    "Custom_Model_cfg/yolov10n.yaml",
+    # "Custom_Model_cfg/yolov10n.yaml",
     "Custom_Model_cfg/yolo12n.yaml",
     "Custom_Model_cfg/MobileNetV4.yaml",
 
